9fb688e92c8c51d6529a722dfc8a2a0de3e72d03
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2024-06-19 12:59:36 -0700

    add chalk from inquirer

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
-
+//                 Project 01: Guessing Number
 import inquirer from "inquirer";
+import figlet from "figlet";
+import chalk from "chalk";
+
+console.log(chalk.magentaBright(figlet.textSync(`
+                            LET'S gUESS
+  `)));
 
 //1) computer will generate a random number
 //2) User input guessing number
@@ -10,15 +16,18 @@ import inquirer from "inquirer";
 
 // const randomNumber = Math.random();
 // console.log(randomNumber);
-const randomNumber = Math.floor(Math.random() * 10 +1);
+const randomNumber = Math.floor(Math.random() * 10 + 1);
 //console.log(randomNumber);
-const answers = await inquirer.prompt([{
-    name:"userGuessdNumber",
+const answers = await inquirer.prompt([
+    {
+    name: "userGuessdNumber",
     type: "number",
-    message:"please guess a number between 1-10",
-}]);
-if (answers.userGuessdNumber === randomNumber){
-    console.log("Congratulations you guessed a right number.");
+    message:chalk.yellow("please guess a number between 1-10"),
+  },
+]);
+if (answers.userGuessdNumber === randomNumber) {
+  console.log(chalk.green("Congratulations you guessed a right number."));
 } else {
-    console.log("You guessed wrong number");
+  console.log(chalk.red("You guessed wrong"));
+  
 }
